a506a9e18dd4b3eaa0dbd5fa3b741d2c2782105b
Oscar Campos <ocampos@adobe.com>
2020-05-11 09:09:49 -0700

    [Breaking Change] Blogs generate under 'blog' directory instead of posts

--- src/cli.js
+++ src/cli.js
@@ -8,7 +8,7 @@ async function makeFile(answers) {
   const isPage = 'BlogPost' !== answers.templateType;
   const rawFileName = (answers.fileName || 'new-file');
   const fileName =  (answers.fileName || 'new-file').replace(/[\W_]+/g, '-').toLowerCase();
-  const fileFolder = isPage ? './' : './posts/';
+  const fileFolder = isPage ? './' : './blog/';
   const fileExtension = isPage ? '.njk' : '.md';
   const fileContents = isPage ? '---\r\n' +
     'layout: layouts/home.njk\r\n' +
@@ -29,18 +29,18 @@ async function makeFile(answers) {
     ' </div>\r\n\r\n' +
     '-->\r\n\r\n' +
     '<div>\r\n\r\n' +
-    '<h1>{{ title }}</h1>\r\n\r\n' +
+    '  <h1>{{ title }}</h1>\r\n\r\n' +
     '</div>'
     :
     '---\r\n' + 
+    'layout: layouts/post.njk\r\n' + 
     'title: \'' + rawFileName + '\'\r\n' + 
+    'tags:\r\n' + 
+    ' - posts\r\n' + 
     'description:\r\n' + 
     'amzn:\r\n' + 
     'tb:\r\n'  +
     'date: ' + blogDate + '\r\n' + 
-    'tags:\r\n' + 
-    ' - posts\r\n' + 
-    'layout: layouts/post.njk\r\n' + 
     'ogTitle: \'' + rawFileName + '\'\r\n' +
     'ogDesc: \r\n' + 
     'ogUrl: \'' + fileName + '\'\r\n' +
@@ -51,11 +51,10 @@ async function makeFile(answers) {
     '   <img\r\n src=""\r\n class="hero-image"\r\n alt="">\r\n' +
     ' </div>\r\n\r\n' +
     '-->\r\n\r\n' +
-    '<div class="slimmer post">\r\n\r\n' +
-    '# {{ title }}\r\n\r\n' +
-    '</div>';
-    const data = new Uint8Array(Buffer.from(fileContents));
+    '\r\n\r\n' +
+    '# {{ title }}\r\n\r\n';
 
+  const data = new Uint8Array(Buffer.from(fileContents));
   const wholeFileName = `${fileFolder}${fileName}${fileExtension}`;
 
   fs.writeFile(wholeFileName, data, { flag: 'w' }, err => {
